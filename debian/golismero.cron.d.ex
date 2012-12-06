1bddd6308e7d19a50471915ed3ab13a9f4ca0807
Devon Kearns <dookie@kali.org>
2012-12-06 06:45:34 -0700

    Commit before patching

--- debian/golismero.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the golismero package
-#
-0 4	* * *	root	[ -x /usr/bin/golismero_maintenance ] && /usr/bin/golismero_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(golismero):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="golismero" command="/usr/bin/golismero"
